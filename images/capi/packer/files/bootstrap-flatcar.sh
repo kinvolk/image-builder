2e1e854305024a8cd7bbb35901be1ad09e0c81fa
Dongsu Park <dongsu@kinvolk.io>
2020-07-09 15:26:05 +0200

    packer: install awscli based on python2
    Since awscli cannot be installed via python3, make bootstrap-flatcar.sh
    install also pypy2, and set the default python version to 2.
    
    Then install awscli from the ansible part.
    To do that, we need to introduce an ansible parameter `python_path`,
    which allows users to specify Flatcar-specific python libs path.
    Also creates python virtualenv `/opt/bin/builder-env`, where the python
    libraries are installed.

--- images/capi/packer/files/bootstrap-flatcar.sh
+++ images/capi/packer/files/bootstrap-flatcar.sh
@@ -4,6 +4,7 @@ set -e
 [[ "$BUILD_NAME" != *"flatcar"* ]] && exit 0
 
 BINDIR="/opt/bin"
+BUILDER_ENV="/opt/bin/builder-env"
 
 mkdir -p $BINDIR
 
@@ -15,10 +16,20 @@ fi
 
 PYPY_VERSION=7.0.0
 
+wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
+mv -n pypy-$PYPY_VERSION-linux_x86_64-portable pypy2
+ln -s ./pypy2/bin/pypy python2
+ln -s ./pypy2/bin/pypy python
+
 wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
 mv -n pypy3.5-$PYPY_VERSION-linux_x86_64-portable pypy3
+ln -s ./pypy3/bin/pypy3 python3
 
-ln -s ./pypy3/bin/pypy3 python
 $BINDIR/python --version
+
+${BINDIR}/pypy2/bin/virtualenv-pypy ${BUILDER_ENV}
+chown -R core ${BUILDER_ENV}
+
+ln -s builder-env/bin/pip ${BINDIR}/pip
 
 touch $BINDIR/.bootstrapped
