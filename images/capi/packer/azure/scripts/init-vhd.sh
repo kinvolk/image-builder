75ec19cef25c614b24fd592d0980b4c39e6c4915
Dongsu Park <dongsu@kinvolk.io>
2021-03-23 16:11:07 +0100

    Revert "TMP: use uploaded Flatcar image for building"
    This reverts commit c23c94e02914435217be968424eecb7579733b95.

--- images/capi/packer/azure/scripts/init-vhd.sh
+++ images/capi/packer/azure/scripts/init-vhd.sh
@@ -15,7 +15,7 @@ export AZURE_LOCATION="${AZURE_LOCATION:-southcentralus}"
 az group create -n ${RESOURCE_GROUP_NAME} -l ${AZURE_LOCATION} --tags ${TAGS:-}
 CREATE_TIME="$(date +%s)"
 RANDOM_SUFFIX="$(head /dev/urandom | LC_ALL=C tr -dc a-z | head -c 4 ; echo '')"
-export STORAGE_ACCOUNT_NAME="${STORAGE_ACCOUNT_NAME:-capi${CREATE_TIME}${RANDOM_SUFFIX}}"
+export STORAGE_ACCOUNT_NAME="capi${CREATE_TIME}${RANDOM_SUFFIX}"
 az storage account check-name --name ${STORAGE_ACCOUNT_NAME}
 az storage account create -n ${STORAGE_ACCOUNT_NAME} -g ${RESOURCE_GROUP_NAME}
 
